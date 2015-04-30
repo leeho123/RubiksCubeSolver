f593043edc33e6087b44d56bf46631201926b220
Le Hoang <leeho_400@hotmail.com>
2015-04-30 16:27:54 +0000

    master

--- robot/masterTest.py
+++ robot/masterTest.py
@@ -5,14 +5,13 @@ port = 12345
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port))
 s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
 while True:
-	data = conn.recv(1024)
-	if not data:
-		print 'Data is invalid' 
-		break
-	conn.sendall(data)
+	conn, addr = s.accept()
+	print('Connected by', addr)
+	conn.sendall('
+	#data = conn.recv(1024)
+	#if not data: continue
+	#conn.sendall(data)
 conn.close()
 
 
